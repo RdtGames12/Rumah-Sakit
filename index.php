cd93e65864d93878f7b6f14ed8e5c92ad8b47cdd
Panturas <rafigamer111@gmail.com>
2024-05-23 21:25:33 +0700

    update

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ session_destroy();
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Pages / Login - Rekam Medis RS Nabil</title>
+  <title>Rekam Medis RS Nabil</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
